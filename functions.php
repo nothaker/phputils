872cd80cbdd149016f4b04939d4f8d76cfe1c29e
a.kazakov <a.kazakov@iceberg.ru>
2019-12-23 15:38:10 +0400

    Add is_guid function

--- functions.php
+++ functions.php
@@ -1,7 +1,48 @@
 <?php
 
-!defined("ICEWORK_UTILS_FIRST_IN_ARRAY") && define("ICEWORK_UTILS_FIRST_IN_ARRAY", "@first");
-!defined("ICEWORK_UTILS_LAST_IN_ARRAY") && define("ICEWORK_UTILS_LAST_IN_ARRAY", "@last");
+define("IW_UTILS_FIRST_IN_ARRAY", "@first");
+define("IW_UTILS_LAST_IN_ARRAY", "@last");
+
+//define("IW_GUID_CASE_INSENSITIVE", 0x00);
+define("IW_GUID_CASE_LOWER", 0x01);
+define("IW_GUID_CASE_UPPER", 0x02);
+define("IW_GUID_FORMAT_BRACKETS", 0x04);
+define("IW_GUID_FORMAT_NO_CHUNK_DIVIDED", 0x08);
+
+/**
+ * Default case insensitive, format - chunks divided by '-' with no brackets.
+ * Setting other formats: IW_GUID_CASE_LOWER or IW_GUID_CASE_UPPER, IW_GUID_FORMAT_BRACKETS, IW_GUID_FORMAT_NO_CHUNK_DIVIDED combined with binary "or" operation
+ * @param $guid
+ * @param int $options IW_GUID_CASE_LOWER, IW_GUID_CASE_UPPER, IW_GUID_FORMAT_BRACKETS, IW_GUID_FORMAT_NO_CHUNK_DIVIDED
+ * @return false|int
+ */
+function is_guid($guid, $options=0) {
+  $case=$options & 0x03;
+  $enableBrackets=$options & IW_GUID_FORMAT_BRACKETS;
+  $enableNoDividers=$options & IW_GUID_FORMAT_NO_CHUNK_DIVIDED;
+
+  $hexChunkPattern=$case>IW_GUID_CASE_LOWER
+    ? 'A-F0-9'
+    : 'a-f0-9';
+
+  $bodyPattern=$enableNoDividers
+    ? '[' . $hexChunkPattern . ']{32}'
+    : implode('\-', [
+      '[' . $hexChunkPattern . ']{8}',
+      '[' . $hexChunkPattern . ']{4}',
+      '[' . $hexChunkPattern . ']{4}',
+      '[' . $hexChunkPattern . ']{4}',
+      '[' . $hexChunkPattern . ']{12}',
+    ]);
+
+  $pattern=
+    '/^' .
+    ($enableBrackets ? '\{' . $bodyPattern . '\}' : $bodyPattern) .
+    '$/' .
+    ($case===0 ? 'i' : ''); // if insensitive case
+
+  return preg_match($pattern, $guid);
+}
 
 /**
  * Find property in object (fake recursive)
